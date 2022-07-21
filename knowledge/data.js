755a2e721b0e934b8419e8fa7a76587ecbcc0296
zk <774511622@qq.com>
2022-07-21 14:29:29 +0800

    知识图谱demo

---
+++ knowledge/data.js
@@ -0,0 +1,22 @@
+var links =
+    [
+        { source: '郑州买恒大早睡交流群', target: '车神', 'rela': '群友', type: 'resolved' },
+        { source: '郑州买恒大早睡交流群', target: '复读鸡儿', 'rela': '群友', type: 'resolved' },
+        { source: '郑州买恒大早睡交流群', target: 'mq', 'rela': '群友', type: 'resolved' },
+        { source: '郑州买恒大早睡交流群', target: '嘻哈', 'rela': '群友', type: 'resolved' },
+        { source: '郑州买恒大早睡交流群', target: 'null', 'rela': '群友', type: 'resolved' },
+        { source: '郑州买恒大早睡交流群', target: '黑蛋', 'rela': '群友', type: 'resolved' },
+        { source: '郑州买恒大早睡交流群', target: '干饭王', 'rela': '群友', type: 'resolved' },
+        { source: '郑州买恒大早睡交流群', target: '程序员', 'rela': '群友', type: 'resolved' },
+        { source: '郑州买恒大早睡交流群', target: '餐馆老哥', 'rela': '群友', type: 'resolved' },
+        { source: '郑州买恒大早睡交流群', target: 'crud', 'rela': '群友', type: 'resolved' },
+        { source: '车神', target: '老司机', 'rela': '职业', type: 'resolved' },
+        { source: '13王', target: '摸鱼', 'rela': '职业', type: 'resolved' },
+        { source: '黑蛋', target: '傻逼', '职业': '职业', type: 'resolved' },
+        { source: '复读鸡儿', target: '前端开发', 'rela': '职业', type: 'resolved' },
+        { source: '嘻哈', target: 'java开发', 'rela': '职业', type: 'resolved' },
+        { source: '餐馆老哥', target: '程序员', 'rela': '职业', type: 'resolved' },
+        { source: 'mq', target: 'java开发', 'rela': '职业', type: 'resolved' },
+        { source: '干饭王', target: '绒羊毛', 'rela': '特长', type: 'resolved' },
+        { source: '车神', target: '黑蛋', 'rela': '喜欢', type: 'resolved' },
+    ];
